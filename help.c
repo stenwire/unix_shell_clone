2c6171182a813e78b49ae2ac0a6bdb8a2220f90d
Stephen Nwankwo <nwankwostephen039@yahoo.com>
2022-05-16 23:08:07 +0100

    [chore]: Add help builtins

---
+++ help.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _help - shows available commands 
+ * @__attribute__: declare unused variable
+ * 
+ * Return: void
+ */
+void _help(vars_t *vars __attribute__((unused)))
+{
+    __attribute__((unused)) void *f;
+    _puts("This is the help screen\n");
+}
